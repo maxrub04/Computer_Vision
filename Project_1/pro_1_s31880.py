72d13484fbd7af3ecd6b9a9ad50abf69125db06c
Maksym Rubtsov <maksrub04@gmail.com>
2026-03-10 14:34:22 +0100

    program 1 final

--- Project_1/pro_1_s31880.py
+++ Project_1/pro_1_s31880.py
@@ -9,47 +9,27 @@ if img is None:
 hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 kernel = np.ones((5, 5), np.uint8)
 
-lower_red = np.array([0, 150, 0], np.uint8)
-upper_red = np.array([15, 255, 255], np.uint8)
-mask0 = cv.inRange(hsv, lower_red, upper_red)
-
-lower_red = np.array([165, 150, 70], np.uint8)
-upper_red = np.array([179, 255, 255], np.uint8)
-mask1 = cv.inRange(hsv, lower_red, upper_red)
+mask0 = cv.inRange(hsv, np.array([0, 150, 0], np.uint8),   np.array([15, 255, 255], np.uint8))
+mask1 = cv.inRange(hsv, np.array([165, 150, 70], np.uint8), np.array([179, 255, 255], np.uint8))
 
 combined_mask = cv.bitwise_or(mask0, mask1)
 combined_mask = cv.morphologyEx(combined_mask, cv.MORPH_OPEN, kernel)
 combined_mask = cv.morphologyEx(combined_mask, cv.MORPH_CLOSE, kernel)
 
-# --- Find contours on the cleaned mask ---
-contours, _ = cv.findContours(combined_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+contours, i = cv.findContours(combined_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
 if contours:
-    # Take the largest contour as the ball
     largest = max(contours, key=cv.contourArea)
-
-    # Draw the contour outline
     cv.drawContours(img, [largest], -1, (255, 255, 255), 2)
 
-    # Calculate centre of gravity using moments
     M = cv.moments(largest)
-    if M["m00"] != 0:
-        cx = int(M["m10"] / M["m00"])
-        cy = int(M["m01"] / M["m00"])
-    else:
-        x, y, w, h = cv.boundingRect(largest)
-        cx, cy = x + w // 2, y + h // 2
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
 
-    # Mark the centre of gravity
     cv.circle(img, (cx, cy), 5, (255, 255, 255), -1)
-
-    # Draw text near the centre of gravity
-    cv.putText(img, 'Red ball', (cx - 40, cy - 20),
-               cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
+    cv.putText(img, 'Red ball', (cx - 40, cy - 20), cv.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
 else:
     print("No red ball detected.")
 
 cv.imshow('Red Ball', img)
 cv.waitKey(0)
-cv.destroyAllWindows()
-
